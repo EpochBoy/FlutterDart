41c9fa19d281ae02ecf2e99679361d341624e1fe
EpochBoy <epochboy@epoch.engineering>
2021-06-23 18:49:13 +0200

    Manipulating Sate

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -3,18 +3,42 @@ import 'package:flutter/material.dart';
 // void main() {
 //   runApp(MyApp());
 // }
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() => {questionIndex = questionIndex + 1});
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal',
+      'What\s your favorite language'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Test App'),
         ),
-        body: Text('Default Text'),
+        body: Column(children: [
+          Text(questions[questionIndex]),
+          ElevatedButton(child: Text('Answer1'), onPressed: answerQuestion),
+          ElevatedButton(child: Text('Answer2'), onPressed: answerQuestion),
+          ElevatedButton(child: Text('Answer3'), onPressed: answerQuestion),
+        ]),
       ),
     );
   }
